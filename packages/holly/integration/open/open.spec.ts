551d42a3a7a8447934316f307a3c704cffcdcddb
Luke Page <luke.a.page@gmail.com>
2020-03-12 08:43:46 +0100

    fix open test

--- packages/holly/integration/open/open.spec.ts
+++ packages/holly/integration/open/open.spec.ts
@@ -9,10 +9,12 @@ describe("Open mode", () => {
     await $(".tst-spec-list").textArray().shouldMatchInlineSnapshot(`[
                 "integration/passes/assertionRetry.spec.ts",
                 "integration/passes/clientSidePromises.spec.ts",
+                "integration/passes/directPlaywrightUsage.spec.ts",
                 "integration/passes/keyboard.spec.ts",
                 "integration/passes/matchers.spec.ts",
                 "integration/passes/misccommands.spec.ts",
                 "integration/passes/mouse.spec.ts",
+                "integration/passes/pipe.spec.ts",
                 "integration/passes/screenshots.spec.ts",
                 "integration/passes/testRetry.spec.ts"
           ]`);
@@ -33,6 +35,13 @@ describe("Open mode", () => {
             "Focus",
             "Disable"
         ]
+    ],
+    [
+        "Mouse / page api / clicks a particular page",
+        [
+            "Focus",
+            "Disable"
+        ]
     ]
 ]`);
 
@@ -43,6 +52,15 @@ describe("Open mode", () => {
     await byText("Start").click();
 
     await byText("Mouse / element api / clicks a element")
+      .$(".tst-command-list")
+      .textArray().shouldMatchInlineSnapshot(`[
+    "click",
+    "text",
+    "shouldEqual"
+]`);
+
+    // For now - we need to wait for the test to complete to stop these tests failing
+    await byText("Mouse / page api / clicks a particular page")
       .$(".tst-command-list")
       .textArray().shouldMatchInlineSnapshot(`[
     "click",
